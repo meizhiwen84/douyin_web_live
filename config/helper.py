85a1bfebf46af69f7d31429c66055d8e106bda63
meizhiwen <meizhiwen@300.cn>
2022-10-24 09:13:25 +0800

    1

--- config/helper.py
+++ config/helper.py
@@ -1,8 +1,11 @@
+import os
+import sys
 from pathlib import Path
 from ruamel import yaml
 
 def config():
-    settings_file = str(Path(__file__).parent.absolute()) + '/settings.yml'
+    # settings_file = str(Path(__file__).parent.absolute()) + '/settings.yml'
+    settings_file = os.path.join(os.path.dirname(os.path.abspath(os.path.relpath(sys.argv[0]))), "config", "settings.yml")
 
     with open(settings_file, 'r') as f:
         return yaml.load(f, Loader=yaml.UnsafeLoader)
